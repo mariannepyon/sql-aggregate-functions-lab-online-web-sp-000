72f3312f65036c521a5a65f08279595dca183838
Marianne Pyon <mariannepyon@gmail.com>
2019-10-03 17:50:55 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,12 +1,15 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
+  SELECT max(gpa) FROM students;
 end
 
 def lowest_student_gpa
+  SELECT min(gpa) FROM students;
 end
 
 def average_student_gpa
+  SELECT AVG()
 end
 
 def total_tardies_for_all_students
